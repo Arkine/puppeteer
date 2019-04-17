7ea6deb68c5c7d66c5633dde9a62a6ac42ad9af0
slyngstad60 <kevan.slyngstad@geckodesigns.com>
2019-04-17 17:07:59 -0600

    made pipline version of the check details test

--- tests/american-express/checkDetails.js
+++ tests/american-express/checkDetails.js
@@ -3,7 +3,10 @@
  * @param {Object} ctx
  */
 
+const assert = require('assert');
+
 module.exports = async (ctx) => {
+	const navPromise = ctx.page.waitForNavigation({ waitUntil: 'domcontentloaded' });
     const viewAllCardsBttn = '#content > page-app-container > on-scroll-container > section > main-container > div.aexp-product-filter > div.aexp-product-catagories-and-tiles > div.aexp-product-tiles > div.aexp-product-tiles__item.dls-icon-cashback.active > div.aexp-product-tiles__item-tiles > div:nth-child(3) > div.contentContainer.purify_newBusinessHomeProductTiles__contentContainer--3EQes > div > a';
     // The card categories buttons
     const cardCategories = '#business-credit-cards-v3 > div.sections-container.vac-page-v2.has-filter-title > div.aexp-card-filter.item-count-8.vac-card-filter.has-title > ul > li.item-unit > h2';
@@ -35,30 +38,60 @@ module.exports = async (ctx) => {
     await ctx.page.waitFor(1000);
 
     // Get the column index of the column we will click
-    await ctx.page.$$eval(rows, rowEls => {
-        // This maps to the column that we will click
-        let colIndex = 0;
-        // Get the row with the fees
-        const feesRow = rowEls[2];
-        // row with the learnMore Bttns
-        const bttnRow = rowEls[4];
+	const fee = await ctx.page.$$eval(rows, rowEls => {
+		// This maps to the column that we will click
+		let colIndex = 0;
+		let fee;
+		// Get the row with the fees
+		const feesRow = rowEls[2];
+		// row with the learnMore Bttns
+		const bttnRow = rowEls[4];
 
-        // The rows containing our data
-        const feeEls = feesRow.querySelectorAll('.fee');
-        const learnMoreBttns = bttnRow.querySelectorAll('.learnmore-button');
+		// Get all of the fee els and learnmore buttons
+		const feeEls = feesRow.querySelectorAll('.fee');
+		const learnMoreBttns = bttnRow.querySelectorAll('.learnmore-button');
 
-        // Find which card has no fee
-        if (feeEls.length) {
-            for (let i = 0; i < feeEls.length; i++) {``
-                if (/\$[1-9]{1,}/g.test(feeEls[i].innerText)) {
-                    colIndex = i;
-                    break;
-                }
-            }
-        }
-        // Click the corresponding learn more bttn
-        learnMoreBttns[colIndex].click();
-    });
+		// Find which card has no fee and store the index
+		if (feeEls.length) {
+			for (let i = 0; i < feeEls.length; i++) {
+				const re = /(\$[1-9]{1,} Annual Fee)/g;
+
+				if (re.test(feeEls[i].innerHTML)) {
+					colIndex = i;
+					fee = feeEls[i].innerHTML.match(re)[0];
+					break;
+				}
+			}
+		}
+
+		// Click the corresponding learn more bttn
+		learnMoreBttns[colIndex].click();
+
+		return fee;
+	});
+
+	// Wait for the page to navigate
+	await navPromise;
+
+	// Wait for the fees title
+	await ctx.page.waitForSelector('.credit-card-detail h2.aexp-feature-title');
+
+	const detailsFee = await ctx.page.$$eval('.credit-card-detail aexp-feature', features => {
+		let index;
+		for (let i = 0; i < features.length; i++) {
+			const header = features[i].querySelector('h2.aexp-feature-title');
+			if (header.innerText === 'Fees') {
+				index = i;
+				break;
+			}
+		}
+
+		const header = features[index].querySelector('aexp-feature-header');
+		return header.innerText;
+	});
+
+	// I wasn't sure whether the assertion should match with casing as well as verbiage so I went for verbiage
+	assert.equal(fee.toLowerCase(), detailsFee.toLowerCase());
 
     await ctx.page.waitFor(1000);
 
